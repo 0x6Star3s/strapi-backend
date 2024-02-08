8370500c3ab7720d50b847a19411eaa59060f43b
Paweł Stach <95292019+0x6Star3s@users.noreply.github.com>
2024-02-08 09:46:53 +0100

    Update page.ts

--- src/api/page/routes/page.ts
+++ src/api/page/routes/page.ts
@@ -4,13 +4,5 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreRouter('api::page.page',{
-    config: {
-      find: {
-        middlewares: ["api::page.page-populate-middleware"]
-      },
-      findOne: {
-        middlewares: ["api::page.page-populate-middleware"]
-      },
-    }
-  });
+export default factories.
+createCoreRouter('api::page.page');
